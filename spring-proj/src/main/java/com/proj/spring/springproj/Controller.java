92b10e36da35363668580bea12476f1733f6f3f2
ensha <ensha@cybercesdatauni>
2020-03-26 19:31:25 -0700

    commit

---
+++ spring-proj/src/main/java/com/proj/spring/springproj/Controller.java
@@ -0,0 +1,15 @@
+package com.proj.spring.springproj;
+
+import java.util.Arrays;
+import java.util.List;
+//
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+//rc
+@RestController
+public class Controller {
+@GetMapping("/Books")
+public List<Book> giveBook(){
+	return Arrays.asList(new Book(1,"enshas book","ensha"));
+}
+}
