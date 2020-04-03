bfbbc0d4d82f212f3457c5b3fb5a159196b97176
ensha <ensha@cybercesdatauni>
2020-04-03 14:03:21 -0700

    commit changess to restcontroller

--- spring-proj/src/main/java/com/proj/spring/springproj/Controller.java
+++ spring-proj/src/main/java/com/proj/spring/springproj/Controller.java
@@ -5,7 +5,7 @@ import java.util.List;
 //
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-//rc
+//restcontroller
 @RestController
 public class Controller {
 @GetMapping("/Books")
